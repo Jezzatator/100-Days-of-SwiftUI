09ff78cd91e897d8cdee1859c45b0b34bce336d7
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-09 16:52:58 +0200

    Création WeSplit - Arrêtassions à Project 1, part one - Creating a form

---
+++ Days 16-24 Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jérémie - Ada on 09/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Group {
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+            }
+            
+            Section {
+                Text("coucou louis")
+                Text("Fuck 1312")
+            }
+            Section {
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+                Text("coucou louis")
+                Text("Fuck 1312")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Days 16-24 Starting SwiftUI/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jérémie - Ada on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
