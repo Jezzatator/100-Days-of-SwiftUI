6e57cb9241e0fc062551e8ace8becb71253f2e19
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-13 16:09:43 +0200

    Project 1, part two : Today you have four topics to work through, in which you’ll apply your knowledge of Form, @State, Picker, and more.
    - Reading text from the user with TextField
    - Creating pickers in a form
    - Adding a segmented control for tip percentages
    - Calculating the total per person
    - Hiding the keyboard

--- Days 16-24 Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
+++ Days 16-24 Starting SwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -8,22 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-let name = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
+
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var  totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
-        Form {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(name, id: \.self) {
-                    Text($0)
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Nombre de personnes", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) personnes")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Pourboire", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Pourboire?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Fini") {
+                        amountIsFocused = false
+                    }
                 }
             }
         }
     }
 }
 
-                     }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
